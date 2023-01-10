b5c789a73cad47f4abc401803ae24abb0c080b42
hewww11 <1099505869@qq.com>
2023-01-10 18:03:18 +0800

    c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int print(int a){
+    printf("test\n");
+    //test
+    return 1;
+}
+
+int main(){
+    print(2);
+    printf ("hello word a\n");
+    printf ("hello word2\n");
+    return 0;
+}
